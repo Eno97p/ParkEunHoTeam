05a33f29db738db0d8762f98589d0e518276e671
eunho park <elnino1997@naver.com>
2024-07-23 16:40:01 +0900

    Re

--- Client/Private/Body_Mantari.cpp
+++ Client/Private/Body_Mantari.cpp
@@ -128,6 +128,17 @@ void CBody_Mantari::Tick(_float fTimeDelta)
 		if (m_iPastAnimIndex == 24)
 		{
 			fAnimSpeed = 1.3f;
+			if (m_pModelCom->Check_CurDuration(0.55))
+			{
+				_matrix Mat = XMLoadFloat4x4(&m_WorldMatrix);
+				_vector vLook = XMVector4Normalize(Mat.r[2]);
+				_vector vPos = Mat.r[3] + vLook * 1.2f;
+				_float4 vStartPos;
+				XMStoreFloat4(&vStartPos, vPos);
+				EFFECTMGR->Generate_Particle(23, vStartPos);
+				EFFECTMGR->Generate_Particle(23, vStartPos, nullptr, XMVectorSet(0.f,1.f,0.f,0.f), 90.f);
+			}
+		}
 		else if(m_iPastAnimIndex == 22)
 			fAnimSpeed = 2.f;
 		else

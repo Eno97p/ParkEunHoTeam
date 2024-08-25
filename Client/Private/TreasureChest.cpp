f25f86ecd7f669dde6c090f12b1b5bf516db317d
김보민 <byj060362@gmail.com>
2024-08-25 22:03:16 +0900

    Weapon / Skill 습득 UI 구현

--- Client/Private/TreasureChest.cpp
+++ Client/Private/TreasureChest.cpp
@@ -114,7 +114,7 @@ void CTreasureChest::Late_Tick(_float fTimeDelta)
 			vPlayerPos = dynamic_cast<CTransform*>(pComponent)->Get_State(CTransform::STATE_POSITION);
 			vSavePointPos = m_pTransformCom->Get_State(CTransform::STATE_POSITION);
 
-			if (fabs(XMVectorGetX(XMVector4Length(vPlayerPos - vSavePointPos)) < 5.0f))
+			if (fabs(XMVectorGetX(XMVector4Length(vPlayerPos - vSavePointPos)) < 2.0f)) // 5.f
 			{
 				if (m_pGameInstance->Key_Down(DIK_F))
 				{
